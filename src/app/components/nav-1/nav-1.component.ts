ecbe4d8898cb8397cb60ddf2b67c42687e315e54
Paolo Martín Cajo Flores <paolocajo09@gmail.com>
2024-06-29 08:24:07 -0500

    login autenticacion

--- src/app/components/nav-1/nav-1.component.ts
+++ src/app/components/nav-1/nav-1.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EstadoService } from '../../services/estado.service';
+import { Estado } from '../../interfaces/Estado';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-1',
   templateUrl: './nav-1.component.html',
-  styleUrl: './nav-1.component.css'
+  styleUrls: ['./nav-1.component.css'],
 })
-export class Nav1Component {
+export class Nav1Component implements OnInit {
+  estado: Estado;
 
+  constructor(private estadoService: EstadoService, private router: Router) {
+    this.estado = this.estadoService.getEstado() || {
+      idUsuario: 0,
+      nombreUsuario: '',
+      authenticated: false,
+    };
+  }
+
+  ngOnInit(): void {
+    console.log(this.estadoService.getEstado());
+  }
+
+  cerrarSesion() {
+    this.estadoService.setEstado({
+      idUsuario: 0,
+      nombreUsuario: '',
+      authenticated: false,
+    });
+    this.estado = { idUsuario: 0, nombreUsuario: '', authenticated: false };
+    this.router.navigate(['/login']);
+  }
 }
---
+++ src/app/interfaces/Estado.ts
@@ -0,0 +1,5 @@
+export interface Estado {
+  idUsuario: number;
+  nombreUsuario: string;
+  authenticated: boolean;
+}
